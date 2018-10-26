434ece7bc63ce27a1fc601c31e1ba0955002cf20
eatrzcinska <eatrzcinska@gmail.com>
2018-10-26 10:04:08 +0200

    change liquibase

--- src/main/resources/07_create_VerificationToken_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS verification_token (
-  id          VARCHAR(36) primary key,
-  token        VARCHAR(60),
-  user_email        VARCHAR(30),
-  expiry_date   TIMESTAMP,
-  FOREIGN KEY (user_email) REFERENCES user(email)
-);
