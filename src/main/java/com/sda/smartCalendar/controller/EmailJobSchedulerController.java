1bc8d920e4a1135500307b7b8742d1f518f444ac
Pawel <swiecpawel@gmail.com>
2018-10-30 11:50:44 +0100

    Quartz   * draft notification form added

--- src/main/java/com/sda/smartCalendar/controller/EmailJobSchedulerController.java
+++ src/main/java/com/sda/smartCalendar/controller/EmailJobSchedulerController.java
@@ -10,23 +10,72 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.TimeZone;
 import java.util.UUID;
 
-@RestController
+@Controller
 public class EmailJobSchedulerController {
     private static final Logger logger = LoggerFactory.getLogger(EmailJobSchedulerController.class);
 
     @Autowired
     private Scheduler scheduler;
 
-    @PostMapping("/scheduleEmail")
+    @GetMapping(value = "/scheduleEmail") //{eventId}
+    public String showForm(Model model) { //@PathVariable("eventId") String eventId
+        ScheduleEmailRequest scheduleEmailRequest = new ScheduleEmailRequest();
+
+
+
+
+        model.addAttribute("schedulerEmailRequest", scheduleEmailRequest);
+        return "notification";
+    }
+
+
+
+
+
+    @PostMapping("/scheduleEmail") //{eventId}
+    public ResponseEntity<ScheduleEmailResponse> scheduleEmail(@Valid @RequestBody @ModelAttribute (value = "schedulerEmailRequest")
+                                         ScheduleEmailRequest scheduleEmailRequest) { //@PathVariable("eventId") String eventId
+        try {
+            TimeZone timeZone = TimeZone.getDefault();
+            scheduleEmailRequest.setTimeZone(timeZone.toZoneId());
+            System.out.println(scheduleEmailRequest.getTimeZone());
+            ZonedDateTime dateTime = ZonedDateTime.of(scheduleEmailRequest.getDateTime(), scheduleEmailRequest.getTimeZone());
+            if(dateTime.isBefore(ZonedDateTime.now())) {
+                ScheduleEmailResponse scheduleEmailResponse = new ScheduleEmailResponse(false,
+                        "dateTime must be after current time");
+                return ResponseEntity.badRequest().body(scheduleEmailResponse);
+            }
+
+            JobDetail jobDetail = buildJobDetail(scheduleEmailRequest);
+            Trigger trigger = buildJobTrigger(jobDetail, dateTime);
+            scheduler.scheduleJob(jobDetail, trigger);
+
+            ScheduleEmailResponse scheduleEmailResponse = new ScheduleEmailResponse(true,
+                    jobDetail.getKey().getName(), jobDetail.getKey().getGroup(), "Email Scheduled Successfully!");
+            return ResponseEntity.ok(scheduleEmailResponse);
+        } catch (SchedulerException ex) {
+            logger.error("Error scheduling email", ex);
+
+            ScheduleEmailResponse scheduleEmailResponse = new ScheduleEmailResponse(false,
+                    "Error scheduling email. Please try later!");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(scheduleEmailResponse);
+        }
+    }
+
+
+
+
+/*    @PostMapping("/scheduleEmail")
     public ResponseEntity<ScheduleEmailResponse> scheduleEmail(@Valid @RequestBody ScheduleEmailRequest scheduleEmailRequest) {
         try {
             ZonedDateTime dateTime = ZonedDateTime.of(scheduleEmailRequest.getDateTime(), scheduleEmailRequest.getTimeZone());
@@ -50,7 +99,7 @@ public class EmailJobSchedulerController {
                     "Error scheduling email. Please try later!");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(scheduleEmailResponse);
         }
-    }
+    }*/
 
     private JobDetail buildJobDetail(ScheduleEmailRequest scheduleEmailRequest) {
         JobDataMap jobDataMap = new JobDataMap();
